04a753616c9e889ed1164e3de6ac4c70b6aa09ac
hatem <hatim2012003@gmail.com>
2026-01-05 18:27:10 +0200

    Initial commit: backend project setup

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,30 @@
-import mongoose from 'mongoose'
-const connectDB  = async ()=>{
-    return await mongoose.connect(process.env.MONGODB_URI)
-    .then(res=>console.log(`DB Connected successfully on .........${process.env.MONGODB_URI} `))
-    .catch(err=>console.log(` Fail to connect  DB.........${err} `))
-}
+// import mongoose from 'mongoose'
+// const connectDB  = async ()=>{
+//     return await mongoose.connect(process.env.MONGODB_URI)
+//     .then(res=>console.log(`DB Connected successfully on .........${process.env.MONGODB_URI} `))
+//     .catch(err=>console.log(` Fail to connect  DB.........${err} `))
+// }
 
+
+// export default connectDB;
+
+
+import mongoose from "mongoose";
+
+mongoose.set("bufferCommands", false);
+
+const connectDB = () => {
+  return mongoose
+    .connect(process.env.MONGODB_URI, {
+      serverSelectionTimeoutMS: 10000,
+    })
+    .then(() => {
+      console.log("DB Connected successfully");
+    })
+    .catch((err) => {
+      console.error("Fail to connect DB:", err.message);
+      throw err;
+    });
+};
 
 export default connectDB;
